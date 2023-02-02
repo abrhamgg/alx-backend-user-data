880e18c5614af049d73dbebae24f20a47ad72612
abrhamgg <abrhamgg@gmail.com>
2023-02-02 23:48:20 +0300

    completed task-6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,13 @@ def hash_password(password: str) -> bytes:
     "function to hash a password salted with byte string"
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    checks if the password matches
+    """
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    else:
+        return False
