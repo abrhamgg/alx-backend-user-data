ea0ed3c532992162642d470c2c23c51ea931f70b
abrhamgg <abrhamgg@gmail.com>
2023-02-07 21:58:01 +0300

    completed task-5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,19 @@ class Auth():
     """Authentication class"""
     def require_auth(self, path: str, excluded_path: List[str]) -> bool:
         """require auth"""
-        return False
+        if not path:
+            return True
+        if not excluded_path:
+            return True
+        path_with_slash = path
+        if path[-1] == '/':
+            path_with_slash = path_with_slash.rstrip(path_with_slash[-1])
+        else:
+            path_with_slash += '/'
+        if path_with_slash in excluded_path or path in excluded_path:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """authorization header"""
