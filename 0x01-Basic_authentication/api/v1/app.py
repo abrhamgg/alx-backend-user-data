4895777c22a17a4ca1edc90feaed88525ce5677d
abrhamgg <abrhamgg@gmail.com>
2023-02-07 21:02:16 +0300

    completed task-2

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -27,6 +27,12 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
+@app.errorhandler(403)
+def forbidded(error) -> str:
+    """forbidded handler"""
+    return jsonify({"error": "Forbidden"})
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
