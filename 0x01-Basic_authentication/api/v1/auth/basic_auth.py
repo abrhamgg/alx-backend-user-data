43f0fb94e48de33f71c0fe5eaa4db95ff1554816
abrhamgg <abrhamgg@gmail.com>
2023-02-07 23:57:19 +0300

    completed task-8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,13 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """BasicAuth inherits from Auth"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        auth = authorization_header.split(' ')
+        if len(auth) != 2 or auth[0] != 'Basic':
+            return None
+        return auth[1]
