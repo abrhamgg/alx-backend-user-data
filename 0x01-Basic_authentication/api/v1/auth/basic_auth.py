1d03864593b64eb627961d31c1c8a51517576c6f
abrhamgg <abrhamgg@gmail.com>
2023-02-08 00:42:10 +0300

    completed task-10:

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Basic Auth"""
 import base64
+from typing import Tuple
 from api.v1.auth.auth import Auth
 
 
@@ -31,3 +32,16 @@ class BasicAuth(Auth):
             return decoded.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                     str) -> tuple:
+        """extract credential from decoded auth header"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) != str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        credentials = decoded_base64_authorization_header.split(':')
+        return credentials[0], credentials[1]
