2eaa2e602b0320aa066818d24d44dc07c61bb571
abrhamgg <abrhamgg@gmail.com>
2023-02-07 23:36:25 +0300

    completed task-7

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,6 +6,7 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.basic_auth import BasicAuth
 import os
 
 
@@ -17,9 +18,11 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 if getenv("AUTH_TYPE") is not None:
     auth = getenv("AUTH_TYPE")
-if auth:
+if auth != 'basic_auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
+else:
+    auth = BasicAuth()
 
 
 @app.before_request
