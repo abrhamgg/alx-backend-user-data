2a28cb75d06026685d3d15758b6d0d45aab754e8
abrhamgg <abrhamgg@gmail.com>
2023-02-09 22:55:10 +0300

    completed task-2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 """Empty session"""
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Session Auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create session method"""
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
