7178262a4aa5efbeb6b9e9c15f8910d7b424e945
abrhamgg <abrhamgg@gmail.com>
2023-02-14 13:27:03 +0300

    completed task-0

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""user service"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """user table"""
+    __tablename__ = 'users'
+    id = Column(String, primary_key=True)
+    email = Column(String(250))
+    hashed_password = Column(String(250))
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
