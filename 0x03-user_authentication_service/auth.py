80d5e1f096eba53ebcd0c1f1c726faa62a363a51
abrhamgg <abrhamgg@gmail.com>
2023-02-15 05:25:25 +0300

    completed task-10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -47,3 +47,15 @@ class Auth:
                 return False
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """takes an email string argument and returns
+        the session ID as a string."""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            user_id = user.id
+            self._db.update_user(user_id, session_id=session_id)
+            return session_id
+        except Exception:
+            return None
