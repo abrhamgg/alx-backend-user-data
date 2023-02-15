00ff3acbceb1716d5b03b63bcb9caf2f60efec9c
abrhamgg <abrhamgg@gmail.com>
2023-02-15 04:31:12 +0300

    completed task-5:

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python3
 """auth module"""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """hash a password and returns a byte"""
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """registers a user and returns a User object"""
+        try:
+            if type(self._db.find_user_by(email=email)) is User:
+                raise ValueError('User ' + email + ' already exists')
+        except NoResultFound:
+            if type(password) is str:
+                password = _hash_password(password)
+            user = self._db.add_user(email, password)
+            return user
--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,10 +35,13 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Adds a new user to the database.
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
-        return new_user
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+            return new_user
+        except Exception:
+            return None
 
     def find_user_by(self, **kwargs) -> User:
         """finds a user by property"""
